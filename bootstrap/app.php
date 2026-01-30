f11c14a4e47a5677cf75cfd62ab520cfe853d6e4
shineistu86 <shineistu86@gmail.com>
2026-01-30 07:41:15 +0700

    Perbaikan konfigurasi aplikasi Osamunime - Memperbaiki error No application encryption key dan error 419 Page Expired

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,13 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+// Load environment variables manually to ensure they're available
+$envPath = dirname(__DIR__).'/.env';
+if (file_exists($envPath)) {
+    $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+    $dotenv->load();
+}
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -16,7 +23,4 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })
-    ->withProviders([
-        App\Providers\DatabaseServiceProvider::class,
-    ])
     ->create();
